00a51592a2eb967382fabd7bc269f6a47dd5c5ca
yusufxon11011 <mabduvaliyev54@gmail.com>
2024-02-26 16:26:26 +0500

    Uyga vazifa

---
+++ keyboards/admin_inline_keyboards.py
@@ -0,0 +1,45 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+from config import DB_NAME
+from utils.database import Database
+
+db = Database(DB_NAME)
+
+def make_category_list() -> InlineKeyboardMarkup:
+    categories = db.get_categories()
+    rows = []
+    for category in categories:
+        rows.append([
+            InlineKeyboardButton(
+                text=str(category[1]),
+                callback_data=str(category[1])
+            )
+        ])
+    kb_categories = InlineKeyboardMarkup(inline_keyboard=rows)
+    return kb_categories
+
+def make_product_list() -> InlineKeyboardMarkup:
+    products = db.get_product()
+    rows = []
+    for product in products:
+        rows.append([
+            InlineKeyboardButton(
+                text=str(product[1]),
+                callback_data=str(product[1])
+            )
+        ])
+    kb_products = InlineKeyboardMarkup(inline_keyboard=rows)
+    return kb_products
+
+def make_ad_list() -> InlineKeyboardMarkup:
+    ads = db.get_my_ads()
+    rows = []
+    for ad in ads:
+        rows.append([
+            InlineKeyboardButton(
+                text=str(ad[1]),
+                callback_data=str(ad[1])
+            )
+        ])
+    kb_ads = InlineKeyboardMarkup(inline_keyboard=rows)
+    return kb_ads
---
+++ keyboards/reg_keyboards.py
@@ -0,0 +1,27 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+
+kb_register = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text="Ro'yxatdan o'tish")
+        ]
+    ],
+    resize_keyboard=True,
+    input_field_placeholder="Iltimos, ro'yxatdan o'tish tugmasini bosing",
+    one_time_keyboard=True
+)
+
+kb_request_contact = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(
+                text='Telefon raqamni yuborish',
+                request_contact=True
+            )
+        ]
+    ],
+    resize_keyboard=True,
+    input_field_placeholder="Iltimos, Telefon raqamni yuborish tugmasini bosing",
+    one_time_keyboard=True
+)
